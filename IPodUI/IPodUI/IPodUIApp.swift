e8ad8d5d2e38a293e3f52933bd18ff2f33cd1345
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2025-04-25 08:07:36 +0300

    Add files via upload

---
+++ IPodUI/IPodUI/IPodUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPodUIApp.swift
+//  IPodUI
+//
+//  Created by Berkan Aydın on 25.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct IPodUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
